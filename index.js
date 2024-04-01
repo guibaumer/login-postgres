0c6277d192195aecd438a69f3f370a58bcbc9140
guibaumer <117869143+guibaumer@users.noreply.github.com>
2024-04-01 18:11:25 -0300

    api url

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ class App {
   
   middlewares() {
     this.app.use((req, res, next) => {
-      // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001'); 
-      res.setHeader('Access-Control-Allow-Origin', ['https://localhost:3001']); 
+      // res.setHeader('Access-Control-Allow-Origin', ['https://localhost:3001']); 
+      // res.setHeader('Access-Control-Allow-Origin', ['http://localhost:3001']); 
+      res.setHeader('Access-Control-Allow-Origin', ['https://master--login-next.netlify.app/']); 
       res.setHeader('Access-Control-Allow-Headers', 'content-type');
       res.setHeader('Access-Control-Allow-Credentials', 'true');
       next();
